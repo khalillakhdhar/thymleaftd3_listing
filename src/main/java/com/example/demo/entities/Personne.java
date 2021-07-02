a32fa1e25bc3e692a048acece343db181dac5362
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-07-02 10:33:25 +0100

    Update Personne.java

--- src/main/java/com/example/demo/entities/Personne.java
+++ src/main/java/com/example/demo/entities/Personne.java
@@ -1,5 +1,41 @@
 package com.example.demo.entities;
 
 public class Personne {
+private String nom,prenom,sexe;
+private int age;
+public Personne(String nom, String prenom, String sexe, int age) {
+	this.nom = nom;
+	this.prenom = prenom;
+	this.sexe = sexe;
+	this.age = age;
+}
+public Personne() {
+}
+public String getNom() {
+	return nom;
+}
+public void setNom(String nom) {
+	this.nom = nom;
+}
+public String getPrenom() {
+	return prenom;
+}
+public void setPrenom(String prenom) {
+	this.prenom = prenom;
+}
+public String getSexe() {
+	return sexe;
+}
+public void setSexe(String sexe) {
+	this.sexe = sexe;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+
+
 
 }
